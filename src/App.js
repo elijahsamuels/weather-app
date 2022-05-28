0f3be31b8d377dc9fecd5fc4be14fcd4acdf066d
elijahsamuels <elijahsamuels@gmail.com>
2022-05-28 11:57:32 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// Global Precipitation Maps
+// https://openweathermap.org/api/global-precipitation-map
+// ENDPOINT:
+// https://maps.openweathermap.org/maps/2.0/radar/{z}/{x}/{y}?appid={API key}&tm={date}
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const api = {
+  key: API_KEY,
+  base: "https://api.openweathermap.org/data/2.5",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location"></div>
+          <div className="date"></div>
+        </div>
+      </main>
     </div>
   );
 }
